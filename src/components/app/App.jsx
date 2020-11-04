e5bedf3c6b790869e4041a9fb1b0a18f0408cd89
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-11-03 17:06:11 -0800

    Add userSearch w/ test

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import UserSearch from '../../user/UserSearch';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <UserSearch />
+    </>
+  );
 }
